abac962177f08e244514b330a11e4c30e986bcd3
Nischal Gautam <gautamnischal2061@gmail.com>
2026-01-06 14:26:55 +0545

    test

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import axios from "axios";
 import cors from "cors";
 import dotenv from "dotenv";
+import axios from 'axios/dist/node/axios.cjs';
 
 dotenv.config();
 
